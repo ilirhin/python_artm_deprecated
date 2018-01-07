ec76446d2f6b1648e3658b78d8af9c5b6cf0f933
Ilya Irkhin <tylorn@tylorn.haze.yandex.net>
2018-01-07 16:20:34 +0300

    twitter experimant

--- artm_utils/dataset_preparations.py
+++ artm_utils/dataset_preparations.py
@@ -8,6 +8,7 @@ import random
 from collections import Counter
 from collections import defaultdict
 from nltk.corpus import stopwords
+import csv
 
 
 def prepare_sklearn_dataset(dataset, calc_cooccurences=False, train_test_split=None, token_2_num=None, process_log_step=500):
@@ -114,7 +115,7 @@ def prepare_sklearn_dataset(dataset, calc_cooccurences=False, train_test_split=N
             )
 
 
-def prepare_nips_dataset(dataset_path, calc_cooccurences=False, train_test_split=None):
+def prepare_nips_dataset(dataset_path, calc_cooccurences=False, train_test_split=None, process_log_step=1000):
     row, col, data = [], [], []
     row_test, col_test, data_test = [], [], []
     not_empty_docs_number = 0
@@ -126,7 +127,7 @@ def prepare_nips_dataset(dataset_path, calc_cooccurences=False, train_test_split
 
     with open(dataset_path, 'r') as f:
         for i, line in enumerate(f.xreadlines()):
-            if i % 1000 == 0:
+            if i % process_log_step == 0:
                 print 'Read file lines:', i
             if i > 0:
                 tokens = line.strip().split(',')
@@ -142,7 +143,113 @@ def prepare_nips_dataset(dataset_path, calc_cooccurences=False, train_test_split
     }
 
     for doc_num, words in documents.iteritems():
-        if doc_num % 100 == 0:
+        if doc_num % process_log_step == 0:
+            print 'Processed documents:', doc_num
+
+        cnt = Counter()
+        cnt_test = Counter()
+
+        for word_num, number in words:
+            for _ in xrange(number):
+                if train_test_split is None or random_gen.random() < train_test_split:
+                    cnt[word_num] += 1
+                else:
+                    cnt_test[word_num] += 1
+
+        if len(cnt) > 0 and (train_test_split is None or len(cnt_test) > 0):
+            for w, c in cnt.iteritems():
+                row.append(not_empty_docs_number)
+                col.append(w)
+                data.append(c)
+
+            for w, c in cnt_test.iteritems():
+                row_test.append(not_empty_docs_number)
+                col_test.append(w)
+                data_test.append(c)
+
+            not_empty_docs_number += 1
+
+            if calc_cooccurences:
+                keys = [x for x, _ in words]
+                doc_cooccurences.update({(w1, w2) for w1 in keys for w2 in keys if w1 != w2})
+                doc_occurences.update(keys)
+
+    print 'Nonzero values:', len(data)
+    shape = (not_empty_docs_number, len(token_2_num))
+
+    if train_test_split is None:
+        if calc_cooccurences:
+            return (
+                scipy.sparse.csr_matrix((data, (row, col))),
+                token_2_num, num_2_token,
+                doc_occurences,
+                doc_cooccurences
+            )
+        else:
+            return (
+                scipy.sparse.csr_matrix((data, (row, col))),
+                token_2_num,
+                num_2_token
+            )
+    else:
+        if calc_cooccurences:
+            return (
+                scipy.sparse.csr_matrix((data, (row, col)), shape=shape),
+                scipy.sparse.csr_matrix((data_test, (row_test, col_test)), shape=shape),
+                token_2_num,
+                num_2_token,
+                doc_occurences,
+                doc_cooccurences
+            )
+        else:
+            return (
+                scipy.sparse.csr_matrix((data, (row, col)), shape=shape),
+                scipy.sparse.csr_matrix((data_test, (row_test, col_test)), shape=shape),
+                token_2_num,
+                num_2_token
+            )
+
+
+def prepare_twitter_sentiment140_dataset(dataset_path, calc_cooccurences=False, train_test_split=None, process_log_step=10000):
+    english_stopwords = set(stopwords.words('english'))
+    occurences = Counter()
+    row, col, data = [], [], []
+    row_test, col_test, data_test = [], [], []
+    not_empty_docs_number = 0
+    doc_cooccurences = Counter()
+    doc_occurences = Counter()
+    random_gen = random.Random(42)
+    token_2_num = {}
+    documents = defaultdict(list)
+
+    with open(dataset_path, 'r') as f:
+        reader = csv.reader(f, delimiter=',', quotechar='"')
+        for i, line in enumerate(reader):
+            doc_target, _, _, _, _, text = line
+            tokens = gensim.utils.lemmatize(text)
+            for token in set(tokens):
+                occurences[token] += 1
+                if token not in english_stopwords:
+                    documents[i].append(token)
+            if i % process_log_step == 0:
+                print 'Preprocessed: ', i
+
+    for i in documents:
+        documents[i] = Counter([word for word in documents[i] if occurences[word] >= 3]).items()
+
+    for i in documents:
+        for (word, count) in documents[i]:
+            if word not in token_2_num:
+                token_2_num[word] = len(token_2_num)
+        documents[i] = [(token_2_num[w], c) for (w, c) in documents[i]]
+
+    num_2_token = {
+        v: k
+        for k, v in token_2_num.iteritems()
+    }
+
+    for doc_num, words in documents.iteritems():
+        if doc_num % process_log_step == 0:
             print 'Processed documents:', doc_num
 
         cnt = Counter()
